8a07a0ef5354c61e0d46dc3018a0e740d900aad9
Royette Manaois <royette.manaois@orangeandbronze.com>
2020-03-11 19:52:27 +0800

    Initial files

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <v-content>
+     
+    </v-content>
+
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null,
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
